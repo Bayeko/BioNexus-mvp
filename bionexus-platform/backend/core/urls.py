4e918dd3eec2f5a767059ceab9f92bd69270ef96
Bayeko <messadene.amir@gmail.com>
2025-08-07 19:42:24 +0200

    Refactor core URLs and routers

--- bionexus-platform/backend/core/urls.py
+++ bionexus-platform/backend/core/urls.py
@@ -4,23 +4,11 @@ from rest_framework.routers import DefaultRouter
 from modules.samples.views import SampleViewSet
 from modules.protocols.views import ProtocolViewSet
 
- codex/refactor-urls-in-backend-core
-
 router = DefaultRouter()
 router.register(r"samples", SampleViewSet)
 router.register(r"protocols", ProtocolViewSet, basename="protocol")
 
-router = DefaultRouter()
-router.register(r'samples', SampleViewSet)
-router.register(r'protocols', ProtocolViewSet, basename='protocol')
- main
-
-
 urlpatterns = [
- codex/refactor-urls-in-backend-core
     path("api/", include(router.urls)),
-
-    path('api/', include(router.urls)),
- main
 ]
 
